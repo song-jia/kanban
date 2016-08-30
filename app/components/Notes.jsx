8da4a79b3e1ead3812642b4f0fc744485f9f7202
Song, Jia <songjia.happycoding@gmail.com>
2016-08-30 11:19:58 +0800

    implement deleting notes

---
+++ app/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Note from './Note';
+
+export default ({notes, onDelete = () => {}}) => (
+  <ul>
+  {
+    notes.map(({id, task}) =>
+      <li key={id}>
+        <Note
+          task={task}
+          onDelete={onDelete.bind(null, id)} />
+      </li>
+    )
+  }
+  </ul>
+)
